b05ad1257ceb6a038eecf190e84b5e8d14d318ea
meanagha <iamanaghapatil@gmail.com>
2021-06-12 18:56:00 +0530

    p4

---
+++ build/precache-manifest.4b2b16668d801b503a60d6879dc068ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77bbe87dde3fe0c97e0064c0f07bf572",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e1c8a0fce379b8f4cdb",
+    "url": "/static/css/2.6b8d264a.chunk.css"
+  },
+  {
+    "revision": "986f4ab8e67131fc0efc",
+    "url": "/static/css/main.b54b21c0.chunk.css"
+  },
+  {
+    "revision": "6e1c8a0fce379b8f4cdb",
+    "url": "/static/js/2.27ee501f.chunk.js"
+  },
+  {
+    "revision": "fcf8eb061440ed54defcf76ae7ffe82a",
+    "url": "/static/js/2.27ee501f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "986f4ab8e67131fc0efc",
+    "url": "/static/js/main.36035892.chunk.js"
+  },
+  {
+    "revision": "2338da9c9c84a3684af8",
+    "url": "/static/js/runtime-main.6b954d51.js"
+  }
+]);
